167959dc390a53bc32dab06829e0e799a11ba1c9
Bria McGee <bria.mcgee@oracle.com>
2023-03-07 11:35:17 -0600

    Add files via upload

---
+++ hide-column-in-sublist/customStubs/Field.js
@@ -0,0 +1,10 @@
+define([], () => {
+  const Field = () => {}
+
+  Field.prototype.updateBreakType = () => {}
+  Field.prototype.updateDisplayType = () => {}
+  Field.prototype.updateDisplaySize = () => {}
+  Field.id = undefined
+  
+  return new Field()
+})
