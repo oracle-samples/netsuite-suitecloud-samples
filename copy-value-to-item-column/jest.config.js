00cdf43a5e2edd1b8899d5f0cf3cba0b61183277
Bria McGee <bria.mcgee@oracle.com>
2023-03-07 11:22:02 -0600

    Add files via upload

---
+++ copy-value-to-item-column/jest.config.js
@@ -0,0 +1,16 @@
+const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration')
+const cliConfig = require('./suitecloud.config')
+
+module.exports = SuiteCloudJestConfiguration.build({
+  'projectFolder': cliConfig.defaultProjectFolder,
+  'projectType': SuiteCloudJestConfiguration.ProjectType.ACP,
+  'customStubs': [
+    {
+      'module': 'N/currentRecord',
+      'path': '<rootDir>/customStubs/currentRecord.js'
+    }, {
+      'module': 'N/currentRecord/instance',
+      'path': '<rootDir>/customStubs/CurrentRecordInstance.js'
+    }
+  ]
+})
