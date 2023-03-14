8a95d388f716f006e4b7c92241237afec2ddb0a8
Bria McGee <bria.mcgee@oracle.com>
2023-03-14 16:52:02 -0500

    Update jest.config.js

---
+++ add-custom-button-to-suitelet/jest.config.js
@@ -0,0 +1,30 @@
+const SuiteCloudJestConfiguration = 
+require(`@oracle/suitecloud-unit-testing/
+  jest-configuration/SuiteCloudJestConfiguration`);
+const cliConfig = require('./suitecloud.config');
+
+module.exports = SuiteCloudJestConfiguration.build({
+    projectFolder: cliConfig.defaultProjectFolder,
+    projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
+    customStubs: [
+        {
+            module: 'N/log',
+            path: '<rootDir>/customStubs/log.js'
+        }, {
+            module: 'N/runtime',
+            path: '<rootDir>/customStubs/runtime.js'
+        }, {
+            module: 'N/runtime/script',
+            path: '<rootDir>/customStubs/Script.js'
+        }, {
+            module: 'N/ui/serverWidget',
+            path: '<rootDir>/customStubs/serverWidget.js'
+        }, {
+            module: 'N/ui/serverWidget/form',
+            path: '<rootDir>/customStubs/Form.js'
+        }, {
+            module: 'N/ui/serverWidget/button',
+            path: '<rootDir>/customStubs/Button.js'
+        }
+    ]
+});
