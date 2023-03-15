3ca30e55dd5b6bbe3e78e0d8cfee6da4aa737d54
Bria McGee <bria.mcgee@oracle.com>
2023-03-15 10:16:53 -0500

    Update jest.config.js

--- salesorder-deposit-tracking/jest.config.js
+++ salesorder-deposit-tracking/jest.config.js
@@ -1,13 +1,13 @@
-/* eslint-disable max-len */
-const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration')
+const SuiteCloudJestConfiguration = require(`@oracle/suitecloud-unit-testing/
+ jest-configuration/SuiteCloudJestConfiguration`)
 
 module.exports = SuiteCloudJestConfiguration.build({
-  'projectFolder': 'src', // or your SDF project folder
-  'projectType': SuiteCloudJestConfiguration.ProjectType.ACP,
-  'customStubs': [
+  projectFolder: 'src', // or your SDF project folder
+  projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
+  customStubs: [
     {
-      'module': 'N/log',
-      'path': '<rootDir>/customStubs/log.js'
+      module: 'N/log',
+      path: '<rootDir>/customStubs/log.js'
     }
   ]
 })
