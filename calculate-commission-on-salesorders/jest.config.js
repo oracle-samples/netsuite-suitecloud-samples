9b4ec18fa71e2bed6fc2caf922ee8148a961a679
Bria McGee <bria.mcgee@oracle.com>
2023-03-15 09:41:36 -0500

    Update jest.config.js

--- calculate-commission-on-salesorders/jest.config.js
+++ calculate-commission-on-salesorders/jest.config.js
@@ -1,21 +1,18 @@
-/*
- * Copyright (c) 2021 Oracle and/or its affiliates.
- * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
- *
- */
-const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration')
+const SuiteCloudJestConfiguration = 
+require(`@oracle/suitecloud-unit-testing/
+ jest-configuration/SuiteCloudJestConfiguration`)
 const cliConfig = require('./suitecloud.config')
 
 module.exports = SuiteCloudJestConfiguration.build({
-  'projectFolder': cliConfig.defaultProjectFolder,
-  'projectType': SuiteCloudJestConfiguration.ProjectType.ACP,
-  'customStubs': [
+  projectFolder: cliConfig.defaultProjectFolder,
+  projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
+  customStubs: [
     {
-      'module': 'N/log',
-      'path': '<rootDir>/customStubs/log.js'
+      module: 'N/log',
+      path: '<rootDir>/customStubs/log.js'
     }, {
-      'module': 'N/error',
-      'path': '<rootDir>/customStubs/error.js'
+      module: 'N/error',
+      path: '<rootDir>/customStubs/error.js'
     }
   ]
 })
