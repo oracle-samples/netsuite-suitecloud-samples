271e107a152f75e02fab08e8cd13db970e36ccce
Bria McGee <bria.mcgee@oracle.com>
2023-03-15 10:08:57 -0500

    Update jest.config.js

--- hide-column-in-sublist/jest.config.js
+++ hide-column-in-sublist/jest.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 const SuiteCloudJestConfiguration = require(`@oracle/suitecloud-unit-testing/
  jest-configuration/SuiteCloudJestConfiguration`)
 const cliConfig = require('./suitecloud.config')
