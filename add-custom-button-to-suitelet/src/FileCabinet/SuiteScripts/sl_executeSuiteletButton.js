e6a369067496254a93013197d7ee34f6c0f8650b
Bria McGee <bria.mcgee@oracle.com>
2023-03-15 09:17:21 -0500

    Update sl_executeSuiteletButton.js

--- add-custom-button-to-suitelet/src/FileCabinet/SuiteScripts/sl_executeSuiteletButton.js
+++ add-custom-button-to-suitelet/src/FileCabinet/SuiteScripts/sl_executeSuiteletButton.js
@@ -5,7 +5,7 @@
  */
 
 define(['N/runtime', 'N/log'], (runtime, log) => {
-    /**
+  /**
    * @param {Object} scriptContext Holds string values for user event 
    * execution contexts.
    * @param {Record} scriptContext.newRecord The new record being loaded.
@@ -13,33 +13,33 @@ define(['N/runtime', 'N/log'], (runtime, log) => {
    * @param {String} scriptContext.type The type of operation 
    * invoked by the event (the trigger type).
    */
-    function beforeLoad (scriptContext) {
-        try {
-            const recCurrent = scriptContext.newRecord;
-            const objForm = scriptContext.form;
-            const stStatus = recCurrent.getValue({
-                fieldId: 'status'
-            });
-            const stSuiteletLinkParam = 
+  function beforeLoad (scriptContext) {
+    try {
+      const recCurrent = scriptContext.newRecord
+      const objForm = scriptContext.form
+      const stStatus = recCurrent.getValue({
+        fieldId: 'status'
+      })
+      const stSuiteletLinkParam = 
               runtime.getCurrentScript().getParameter({
-                  name: 'custscript_suiteletlink'
-              });
-            const suiteletURL = '"' + stSuiteletLinkParam + '"';
-            if (stStatus === 'Pending Fulfillment') {
-                objForm.addButton({
-                    id: 'custpage_suiteletbutton',
-                    label: 'Open Suitelet',
-                    functionName: 'window.open(' + suiteletURL + ')'
-                });
-            }
-        } catch (error) {
-            log.error({
-                title: 'beforeLoad_addButton',
-                details: error.message
-            });
-        }
+                name: 'custscript_suiteletlink'
+              })
+      const suiteletURL = '"' + stSuiteletLinkParam + '"'
+      if (stStatus === 'Pending Fulfillment') {
+        objForm.addButton({
+          id: 'custpage_suiteletbutton',
+          label: 'Open Suitelet',
+          functionName: 'window.open(' + suiteletURL + ')'
+        })
+      }
+    } catch (error) {
+      log.error({
+        title: 'beforeLoad_addButton',
+        details: error.message
+      })
     }
-    return {
-        beforeLoad: beforeLoad
-    };
-});
+  }
+  return {
+    beforeLoad: beforeLoad
+  }
+})
