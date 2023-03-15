6823d50010213c61c0c95295ca3acb7e2bc871b6
Bria McGee <bria.mcgee@oracle.com>
2023-03-15 10:54:17 -0500

    Update jest.config.js

--- set-item-amount-to-zero/jest.config.js
+++ set-item-amount-to-zero/jest.config.js
@@ -1,14 +1,14 @@
-/* eslint-disable max-len */
-const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration')
+const SuiteCloudJestConfiguration = require(`@oracle/suitecloud-unit-testing/
+ jest-configuration/SuiteCloudJestConfiguration`)
 const cliConfig = require('./suitecloud.config')
 
 module.exports = SuiteCloudJestConfiguration.build({
-  'projectFolder': cliConfig.defaultProjectFolder,
-  'projectType': SuiteCloudJestConfiguration.ProjectType.ACP,
-  'customStubs': [
+  projectFolder: cliConfig.defaultProjectFolder,
+  projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
+  customStubs: [
     {
-      'module': 'N/log',
-      'path': '<rootDir>/customStubs/log.js'
+      module: 'N/log',
+      path: '<rootDir>/customStubs/log.js'
     }
   ]
 })
