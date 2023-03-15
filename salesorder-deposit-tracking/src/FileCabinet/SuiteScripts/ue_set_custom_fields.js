91f486b5a7bc5ed8e016f6c68f5c502d3f0a6443
Bria McGee <bria.mcgee@oracle.com>
2023-03-15 10:14:14 -0500

    Update ue_set_custom_fields.js

--- salesorder-deposit-tracking/src/FileCabinet/SuiteScripts/ue_set_custom_fields.js
+++ salesorder-deposit-tracking/src/FileCabinet/SuiteScripts/ue_set_custom_fields.js
@@ -16,64 +16,64 @@ define(['N/record', 'N/search', 'N/log'], (record, search, log) => {
     const contextOrder = scriptContext.newRecord
     const soID = contextOrder.id
     const salesorder = record.load({
-      'type': record.Type.SALES_ORDER,
-      'id': soID
+      type: record.Type.SALES_ORDER,
+      id: soID
     })
     const soTotal = salesorder.getValue({
-      'fieldId': 'total'
+      fieldId: 'total'
     })
     const soEntity = salesorder.getValue({
-      'fieldId': 'entity'
+      fieldId: 'entity'
     })
     const soTranId = salesorder.getValue({
-      'fieldId': 'tranid'
+      fieldId: 'tranid'
     })
     const soFullTextTranID = 'Order #' + soTranId
     const mySearch = search.load({
-      'id': 'customsearch_sobalancedue'
+      id: 'customsearch_sobalancedue'
     })
     const entityFilter = search.createFilter({
-      'name': 'entity',
-      'operator': search.Operator.ANYOF,
-      'values': soEntity
+      name: 'entity',
+      operator: search.Operator.ANYOF,
+      values: soEntity
     })
     const soIdFilter = search.createFilter({
-      'name': 'formulatext',
-      'operator': search.Operator.IS,
-      'formula': `case when {type}='Customer Deposit' 
+      name: 'formulatext',
+      operator: search.Operator.IS,
+      formula: `case when {type}='Customer Deposit' 
         then {appliedtotransaction} 
         when {type}='Deposit Application' then {createdfrom.salesorder} 
         when {type}='Sales Order' then 'Sales Order #'||{number} end`,
-      'values': soFullTextTranID
+      values: soFullTextTranID
     })
     mySearch.filters.push(entityFilter, soIdFilter)
     mySearch.run().each((soresults) => {
       const soTextID = soresults.getValue({
-        'name': 'formulatext',
-        'summary': search.Summary.GROUP
+        name: 'formulatext',
+        summary: search.Summary.GROUP
       })
       if (soFullTextTranID === soTextID) {
         const totalPaid = soresults.getValue({
-          'name': 'formulacurrency',
-          'summary': search.Summary.SUM
+          name: 'formulacurrency',
+          summary: search.Summary.SUM
         })
         const remainingBalanceOnOrder = soTotal - totalPaid
         salesorder.setValue({
-          'fieldId': 'custbody_total_deposit_paid',
-          'value': totalPaid
+          fieldId: 'custbody_total_deposit_paid',
+          value: totalPaid
         })
         salesorder.setValue({
-          'fieldId': 'custbody_balance_remaining',
-          'value': remainingBalanceOnOrder
+          fieldId: 'custbody_balance_remaining',
+          value: remainingBalanceOnOrder
         })
         salesorder.save({
-          'enableSourcing': true,
-          'ignoreMandatoryFields': true
+          enableSourcing: true,
+          ignoreMandatoryFields: true
         })
       }
     })
   }
   return {
-    'afterSubmit': afterSubmit
+    afterSubmit: afterSubmit
   }
 }) 
