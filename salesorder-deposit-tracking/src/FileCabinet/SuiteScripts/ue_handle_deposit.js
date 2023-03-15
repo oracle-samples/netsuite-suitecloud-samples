597e9e4b1ea4529726ab2cca7443660e75d4860f
Bria McGee <bria.mcgee@oracle.com>
2023-03-15 10:13:25 -0500

    Update ue_handle_deposit.js

--- salesorder-deposit-tracking/src/FileCabinet/SuiteScripts/ue_handle_deposit.js
+++ salesorder-deposit-tracking/src/FileCabinet/SuiteScripts/ue_handle_deposit.js
@@ -15,77 +15,77 @@ define(['N/record', 'N/search', 'N/log'], (record, search, log) => {
   function afterSubmit (scriptContext) {
     const contextDepApp = scriptContext.oldRecord
     const soEntity = contextDepApp.getValue({
-      'fieldId': 'customer'
+      fieldId: 'customer'
     })
     const createdFrom = contextDepApp.getValue({
-      'fieldId': 'deposit'
+      fieldId: 'deposit'
     })
     const cusDeposit = record.load({
-      'type': record.Type.CUSTOMER_DEPOSIT,
-      'id': createdFrom,
-      'isDynamic': true
+      type: record.Type.CUSTOMER_DEPOSIT,
+      id: createdFrom,
+      isDynamic: true
     })
     const orderId = cusDeposit.getValue({
-      'fieldId': 'salesorder'
+      fieldId: 'salesorder'
     })
     const soFullTextTranID = cusDeposit.getText({
-      'fieldId': 'salesorder'
+      fieldId: 'salesorder'
     })
     const mySearch = search.load({
-      'id': 'customsearch_sobalancedue'
+      id: 'customsearch_sobalancedue'
     })
     const entityFilter = search.createFilter({
-      'name': 'internalidnumber',
-      'operator': search.Operator.IS,
-      'values': soEntity
+      name: 'internalidnumber',
+      operator: search.Operator.IS,
+      values: soEntity
     })
     const soIdFilter = search.createFilter({
-      'name': 'formulatext',
-      'operator': search.Operator.IS,
-      'formula': `case when {type}='Customer Deposit' 
+      name: 'formulatext',
+      operator: search.Operator.IS,
+      formula: `case when {type}='Customer Deposit' 
         then {appliedtotransaction} 
         when {type}='Deposit Application' then {createdfrom.salesorder} 
         when {type}='Sales Order' then 'Sales Order #'||{number} end`,
-      'values': soFullTextTranID
+      values: soFullTextTranID
     })
     mySearch.filters.push(entityFilter, soIdFilter)
     mySearch.run().each((soresults) => {
       const soTextID = soresults.getValue({
-        'name': 'formulatext',
-        'summary': search.Summary.GROUP
+        name: 'formulatext',
+        summary: search.Summary.GROUP
       })
       if (soFullTextTranID === soTextID) {
         const totalPaid = soresults.getValue({
-          'name': 'formulacurrency',
-          'summary': search.Summary.SUM
+          name: 'formulacurrency',
+          summary: search.Summary.SUM
         })
         const salesorder = record.load({
-          'type': record.Type.SALES_ORDER,
-          'id': orderId,
-          'isDynamic': true
+          type: record.Type.SALES_ORDER,
+          id: orderId,
+          isDynamic: true
         })
         const soTotal = salesorder.getValue({
-          'fieldId': 'total'
+          fieldId: 'total'
         })
         let remainingBalanceOnOrder = parseFloat(soTotal)
         remainingBalanceOnOrder = (parseFloat(remainingBalanceOnOrder) - 
           parseFloat(totalPaid))
         salesorder.setValue({
-          'fieldId': 'custbody_total_deposit_paid',
-          'value': totalPaid
+          fieldId: 'custbody_total_deposit_paid',
+          value: totalPaid
         })
         salesorder.setValue({
-          'fieldId': 'custbody_balance_remaining',
-          'value': remainingBalanceOnOrder
+          fieldId: 'custbody_balance_remaining',
+          value: remainingBalanceOnOrder
         })
         salesorder.save({
-          'enableSourcing': true,
-          'ignoreMandatoryFields': true
+          enableSourcing: true,
+          ignoreMandatoryFields: true
         })
       }
     })
   }
   return {
-    'afterSubmit': afterSubmit
+    afterSubmit: afterSubmit
   }
 }) 
