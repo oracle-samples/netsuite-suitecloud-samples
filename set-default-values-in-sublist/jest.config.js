e870dc500d825fbd8f5f8336fc30c60ed3dee9a9
Bria McGee <bria.mcgee@oracle.com>
2023-03-15 10:49:19 -0500

    Update jest.config.js

--- set-default-values-in-sublist/jest.config.js
+++ set-default-values-in-sublist/jest.config.js
@@ -1,20 +1,20 @@
-/* eslint-disable max-len */
-const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration')
+const SuiteCloudJestConfiguration = require(`@oracle/suitecloud-unit-testing/
+ jest-configuration/SuiteCloudJestConfiguration`)
 const cliConfig = require('./suitecloud.config')
 
 module.exports = SuiteCloudJestConfiguration.build({
-  'projectFolder': cliConfig.defaultProjectFolder,
-  'projectType': SuiteCloudJestConfiguration.ProjectType.ACP,
-  'customStubs': [
+  projectFolder: cliConfig.defaultProjectFolder,
+  projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
+  customStubs: [
     {
-      'module': 'N/log',
-      'path': '<rootDir>/customStubs/log.js'
+      module: 'N/log',
+      path: '<rootDir>/customStubs/log.js'
     }, {
-      'module': 'N/currentRecord',
-      'path': '<rootDir>/customStubs/currentRecord.js'
+      module: 'N/currentRecord',
+      path: '<rootDir>/customStubs/currentRecord.js'
     }, {
-      'module': 'N/currentRecord/instance',
-      'path': '<rootDir>/customStubs/CurrentRecordInstance.js'
+      module: 'N/currentRecord/instance',
+      path: '<rootDir>/customStubs/CurrentRecordInstance.js'
     }
   ]
 })
