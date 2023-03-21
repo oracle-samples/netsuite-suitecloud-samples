e13945c90d45a371857e3988214e407f439fa2cd
Bria McGee <bria.mcgee@oracle.com>
2023-03-21 08:44:49 -0500

    Update jest.config.js

--- calculate-commission-on-salesorders/jest.config.js
+++ calculate-commission-on-salesorders/jest.config.js
@@ -1,6 +1,5 @@
 const SuiteCloudJestConfiguration = 
-require(`@oracle/suitecloud-unit-testing/
- jest-configuration/SuiteCloudJestConfiguration`)
+require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration')
 const cliConfig = require('./suitecloud.config')
 
 module.exports = SuiteCloudJestConfiguration.build({
