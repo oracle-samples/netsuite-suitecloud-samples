e4030661e3d510a870e44c37b1a7a21b87a6bc3d
Bria McGee <bria.mcgee@oracle.com>
2023-03-21 08:49:04 -0500

    Update jest.config.js

--- set-default-values-in-sublist/jest.config.js
+++ set-default-values-in-sublist/jest.config.js
@@ -1,5 +1,4 @@
-const SuiteCloudJestConfiguration = require(`@oracle/suitecloud-unit-testing/
- jest-configuration/SuiteCloudJestConfiguration`)
+const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration')
 const cliConfig = require('./suitecloud.config')
 
 module.exports = SuiteCloudJestConfiguration.build({
