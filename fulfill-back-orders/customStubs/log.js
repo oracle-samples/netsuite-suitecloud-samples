de82a52cc65713fba2e2acd0fd6edd67f0cf818e
Bria McGee <bria.mcgee@oracle.com>
2023-07-18 09:07:30 -0500

    Delete fulfill-back-orders/customStubs directory

--- fulfill-back-orders/customStubs/log.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-define([], () => {
-  const log = () => {}
-  log.prototype.error = () => {}
-  log.prototype.audit = () => {}
-  // eslint-disable-next-line new-cap
-  return new log()
-})
