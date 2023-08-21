4616d92e04cf2c6d0d96843e1903e96f94276579
Bria McGee <bria.mcgee@oracle.com>
2023-08-21 09:43:55 -0500

    Create suitecloud.config.js

---
+++ validate-order-on-entry/suitecloud.config.js
@@ -0,0 +1,16 @@
+const SuiteCloudJestUnitTestRunner =
+require('@oracle/suitecloud-unit-testing/services/SuiteCloudJestUnitTestRunner')
+
+module.exports = {
+  defaultProjectFolder: 'src',
+  commands: {
+    'project:deploy': {
+      beforeExecuting: async args => {
+        await SuiteCloudJestUnitTestRunner.run({
+          // Jest configuration options.
+        })
+        return args
+      }
+    }
+  }
+}
