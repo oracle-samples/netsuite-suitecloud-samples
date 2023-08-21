af26ec392836f8dbc00a335e09f3dcebf295f7bf
Bria McGee <bria.mcgee@oracle.com>
2023-08-21 09:46:19 -0500

    Update jest.config.js

--- disable-tax-fields/jest.config.js
+++ disable-tax-fields/jest.config.js
@@ -4,29 +4,5 @@ const cliConfig = require('./suitecloud.config')
 
 module.exports = SuiteCloudJestConfiguration.build({
   projectFolder: cliConfig.defaultProjectFolder,
-  projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
-  customStubs: [
-    {
-      module: 'N/log',
-      path: '<rootDir>/customStubs/log.js'
-    }, {
-      module: 'N/runtime',
-      path: '<rootDir>/customStubs/runtime.js'
-    }, {
-      module: 'N/runtime/script',
-      path: '<rootDir>/customStubs/Script.js'
-    }, {
-      module: 'N/runtime/user',
-      path: '<rootDir>/customStubs/User.js'
-    }, {
-      module: 'N/currentRecord',
-      path: '<rootDir>/customStubs/currentRecord.js'
-    }, {
-      module: 'N/currentRecord/instance',
-      path: '<rootDir>/customStubs/CurrentRecordInstance.js'
-    }, {
-      module: 'N/currentRecord/field',
-      path: '<rootDir>/customStubs/Field.js'
-    }
-  ]
+  projectType: SuiteCloudJestConfiguration.ProjectType.ACP
 })
