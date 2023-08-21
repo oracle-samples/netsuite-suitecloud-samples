982133ffcf85d62f4f0da0994e4bc99325f75eb0
Bria McGee <bria.mcgee@oracle.com>
2023-08-21 09:30:43 -0500

    Update jest.config.js

--- hide-column-in-sublist/jest.config.js
+++ hide-column-in-sublist/jest.config.js
@@ -3,23 +3,5 @@ const cliConfig = require('./suitecloud.config')
 
 module.exports = SuiteCloudJestConfiguration.build({
   projectFolder: cliConfig.defaultProjectFolder,
-  projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
-  customStubs: [
-    {
-      module: 'N/log',
-      path: '<rootDir>/customStubs/log.js'
-    }, {
-      module: 'N/ui/serverWidget',
-      path: '<rootDir>/customStubs/serverWidget.js'
-    }, {
-      module: 'N/ui/serverWidget/form',
-      path: '<rootDir>/customStubs/Form.js'
-    }, {
-      module: 'N/ui/serverWidget/field',
-      path: '<rootDir>/customStubs/Field.js'
-    }, {
-      module: 'N/ui/serverWidget/sublist',
-      path: '<rootDir>/customStubs/Sublist.js'
-    }
-  ]
+  projectType: SuiteCloudJestConfiguration.ProjectType.ACP
 })
