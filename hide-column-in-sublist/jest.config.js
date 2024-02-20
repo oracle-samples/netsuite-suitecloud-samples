b4fca44de4a5da0747f2fe97c77ae958dcfdeff5
Bria McGee <bria.mcgee@oracle.com>
2024-02-20 10:27:49 -0600

    removing stubs dependency

--- hide-column-in-sublist/jest.config.js
+++ hide-column-in-sublist/jest.config.js
@@ -1,10 +1,8 @@
 const SuiteCloudJestConfiguration =
 require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration')
 const cliConfig = require('./suitecloud.config')
-const SuiteCloudJestStubs = require('suitecloud-unit-testing-stubs/SuiteCloudJestStubs')
  
 module.exports = SuiteCloudJestConfiguration.build({
   projectFolder: cliConfig.defaultProjectFolder,
-  projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
-  customStubs: SuiteCloudJestStubs.customStubs
+  projectType: SuiteCloudJestConfiguration.ProjectType.ACP
 })
