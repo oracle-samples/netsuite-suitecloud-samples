48b1eb12bd2be2b95a02ca958d2a4020335c616b
Erick Cifuentes <erick.cifuentes@oracle.com>
2025-10-16 11:04:18 -0400

    Adhering to new file structure and linting rules before merging to main!

--- add-button-using-workflow/jest.config.js
+++
@@ -1,7 +0,0 @@
-const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration');
-const cliConfig = require('./suitecloud.config');
-
-module.exports = SuiteCloudJestConfiguration.build({
-    projectFolder: cliConfig.defaultProjectFolder,
-    projectType: SuiteCloudJestConfiguration.ProjectType.ACP
-});
